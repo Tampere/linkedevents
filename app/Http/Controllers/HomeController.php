5de32975d4e8aa0bee642e648cd8d64511a09d5a
Antti Sand <antti.sand@tampere.fi>
2017-06-19 10:39:06 +0300

    Clean up commented old code

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -7,16 +7,5 @@ class HomeController extends Controller
     public function index()
     {
         return view('slate');
-        /*return [
-            "keyword" => url('keyword'),
-            //"keyword_set" => url('keyword_set'),
-            "place" => url('place'),
-            //"language" => url('language'),
-            //"organization" => url('organization'),
-            //"image" => url('image'),
-            "event" => url('event'),
-            "search" => url('search'),
-            //"user" => url('user')
-        ];*/
     }
 }
